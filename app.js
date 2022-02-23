8b53bd013625a3be797e8ad62212ad8d4f67ac93
bmsani <bmsani6@gmail.com>
2022-02-23 11:36:26 +0600

    country name and capital showing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const loadCountries = () => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => displayCountries(data))
-}
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
 loadCountries();
-const displayCountries = countries => {
-    for(const country of countries){
-        console.log(country.name.common);
-    }
-}
+const displayCountries = (countries) => {
+  const countriesDiv = document.getElementById("countries");
+  countries.forEach((country) => {
+    const div = document.createElement('div')
+    div.classList.add('country')
+    const countryName = document.createElement('h3')
+    countryName.innerText = country.name.common
+    div.appendChild(countryName)
+    const p = document.createElement('p')
+    p.innerText = country.capital
+    div.appendChild(p)
+    countriesDiv.appendChild(div)
+  });
+};
