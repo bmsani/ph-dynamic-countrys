7172ecf73d8bbd314265e1b870bc27ed086d1fee
bmsani <bmsani6@gmail.com>
2022-02-24 10:55:44 +0600

    National falg and details are showing in the details section

--- app.js
+++ app.js
@@ -7,14 +7,32 @@ loadCountries();
 const displayCountries = (countries) => {
   const countriesDiv = document.getElementById("countries");
   countries.forEach((country) => {
-    const div = document.createElement('div')
-    div.classList.add('country')
-    const countryName = document.createElement('h3')
-    countryName.innerText = country.name.common
-    div.appendChild(countryName)
-    const p = document.createElement('p')
-    p.innerText = country.capital
-    div.appendChild(p)
-    countriesDiv.appendChild(div)
+    const div = document.createElement("div");
+    div.classList.add("country");
+    div.innerHTML = `
+    <h3>${country.name.common}</h3>
+    <p>${country.capital}</p>
+    <button onclick="loadCountryDetails('${country.name.common}')">Details</button>
+    `;
+    countriesDiv.appendChild(div);
   });
 };
+
+const loadCountryDetails = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountryData(data[0]));
+};
+
+
+const displayCountryData = country =>{
+  const detailsDiv = document.getElementById('country-details')
+  detailsDiv.innerHTML = `
+  <h5>Country Name: ${country.name.common}</h5>
+  <h5>Population: ${country.population}</h5>
+  <img src="${country.flags.png}" width="200">
+  `
+  console.log(country.flags);
+
+}
